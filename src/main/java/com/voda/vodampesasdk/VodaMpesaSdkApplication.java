e2622d67634a314af855754d801262cfa9e5cc15
christopheroigo <oigoc60@gmail.com>
2026-09-22 12:04:31 +0300

    all

---
+++ src/main/java/com/voda/vodampesasdk/VodaMpesaSdkApplication.java
@@ -0,0 +1,13 @@
+package com.voda.vodampesasdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodaMpesaSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VodaMpesaSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voda/vodampesasdk/VodaMpesaSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voda.vodampesasdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodaMpesaSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
